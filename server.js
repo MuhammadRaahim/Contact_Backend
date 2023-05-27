d08e2f896328d37491a99218531339648bbf7dd1
MuhammadRaahim <129032061+mRaahim1122@users.noreply.github.com>
2023-05-27 14:29:44 +0500

    index

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.set('view engine', 'ejs');
+app.set(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,5 +25,9 @@ app.listen(port, () => {
 
 app.use('/api', contactRounter);
 app.use('/api/user', userRounter);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
